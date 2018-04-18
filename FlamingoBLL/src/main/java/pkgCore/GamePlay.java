92de52bff43b6ca29366b5d60081164215484082
Omar Ahmad <eatmorerice@outlook.com>
2018-04-18 08:39:20 -0400

    Changed GamePlay class

--- FlamingoBLL/src/main/java/pkgCore/GamePlay.java
+++ FlamingoBLL/src/main/java/pkgCore/GamePlay.java
@@ -19,15 +19,17 @@ public abstract class GamePlay {
 	
 	protected void AddPlayersToGame(ArrayList<Player> Players)
 	{
-		//TODO: Implement this method
+		for(Player p : Players)
+			hmGamePlayers.put(p.getPlayerID(),p);
 	}
 	protected void RemovePlayerFromGame(Player p)
 	{
-		//TODO: Implement this method		
+		hmGamePlayers.remove(p.getPlayerID());
 	}
 	protected Player GetPlayerInGame(Player p)
 	{
-		//TODO: Implement this method	
-		return null;
+		//TODO: test this
+		if(hmGamePlayers.containsKey(p.getPlayerID())) return p;
+		else return null;
 	}
 }
